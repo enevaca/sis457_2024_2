045928335fd5ce46e8d197e5573708660a4f167d
Noel Vaca <noel.vm.2501@gmail.com>
2024-11-25 22:21:20 -0400

    feat: ejemplo básico compra y compra detalle

---
+++ Minerva/WebMinerva/Controllers/ComprasController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebMinerva.Models;
+
+namespace WebMinerva.Controllers
+{
+    public class ComprasController : Controller
+    {
+        private readonly MinervaContext _context;
+
+        public ComprasController(MinervaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Compras
+        public async Task<IActionResult> Index()
+        {
+            var minervaContext = _context.Compras
+                .Include(c => c.IdProveedorNavigation)
+                .Include(c => c.CompraDetalles)
+                .ThenInclude(cd => cd.IdProductoNavigation);
+            return View(await minervaContext.ToListAsync());
+        }
+
+        // GET: Compras/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras
+                .Include(c => c.IdProveedorNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        // GET: Compras/Create
+        public IActionResult Create()
+        {
+            ViewData["IdProveedor"] = new SelectList(_context.Proveedors, "Id", "RazonSocial");
+            ViewBag.Productos = _context.Productos.Select(p => new { p.Id, p.Descripcion }).ToList();
+            var compra = new Compra();
+            compra.CompraDetalles = new List<CompraDetalle>();
+            return View(compra);
+        }
+
+        // POST: Compras/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Compra compra)
+        {
+            if (compra.IdProveedor > 0)
+            {
+                _context.Add(compra);
+                await _context.SaveChangesAsync();
+
+                foreach (var detalle in compra.CompraDetalles)
+                {
+                    if (detalle != null)
+                    {
+                        detalle.Id = 0;
+                        detalle.IdCompra = compra.Id;
+                        _context.Add(detalle);
+                        await _context.SaveChangesAsync();
+
+                        var producto = await _context.Productos.FindAsync(detalle.IdProducto);
+                        if (producto != null)
+                        {
+                            producto.Saldo += detalle.Cantidad;
+                            await _context.SaveChangesAsync();
+                        }
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdProveedor"] = new SelectList(_context.Proveedors, "Id", "RazonSocial", compra.IdProveedor);
+            ViewBag.Productos = _context.Productos.Select(p => new { p.Id, p.Descripcion }).ToList();
+            return View(compra);
+        }
+
+        // GET: Compras/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras.FindAsync(id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdProveedor"] = new SelectList(_context.Proveedors, "Id", "Id", compra.IdProveedor);
+            return View(compra);
+        }
+
+        // POST: Compras/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdProveedor,Transaccion,Fecha,UsuarioRegistro,FechaRegistro,Estado")] Compra compra)
+        {
+            if (id != compra.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(compra);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompraExists(compra.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdProveedor"] = new SelectList(_context.Proveedors, "Id", "Id", compra.IdProveedor);
+            return View(compra);
+        }
+
+        // GET: Compras/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras
+                .Include(c => c.IdProveedorNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        // POST: Compras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var compra = await _context.Compras.FindAsync(id);
+            if (compra != null)
+            {
+                _context.Compras.Remove(compra);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompraExists(int id)
+        {
+            return _context.Compras.Any(e => e.Id == id);
+        }
+    }
+}
